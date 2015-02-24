8761f5744c7ff8d8fbba5b2e704022242e6ab09a
Joshua <joshuaatteberry@gmail.com>
2015-02-24 10:17:14 -0800

    add working permission slip functionality

--- controllers/FieldTripsController.js
+++ controllers/FieldTripsController.js
@@ -1,5 +1,6 @@
-studentRoster.controller('FieldTripsCtrl', function FieldTripsCtrl($scope) {
-  $scope.addStudentWithPermission = function(student) {
+studentRoster.controller('FieldTripsCtrl', function FieldTripsCtrl($scope, StudentsFactory) {
+  $scope.students = StudentsFactory.students;
+  $scope.addStudentWithPermissionSlip = function(student) {
     student.permissionSlip = true;
   };
 });
